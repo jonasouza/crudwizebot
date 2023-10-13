40661be331a623c528bf83141afe972a2e8d3320
charleslukes <charleslukes28@gmail.com>
2023-10-13 11:58:43 +0100

    update migration script

--- api/migrations/1697172590998_create_table_usuarios.cjs
+++ api/migrations/1697172590998_create_table_usuarios.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE usuarios (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, fone VARCHAR(255) NOT NULL, data_nascimento INT NOT NULL)",
+    "up": "CREATE TABLE usuarios (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL, fone VARCHAR(255) NOT NULL, plano VARCHAR(255) NOT NULL)",
     "down": "DROP TABLE usuarios"
 }
